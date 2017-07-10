3e4bdd83c1f606967583f61fff71286b17968ea2
lsetiawan <landungs@uw.edu>
2017-07-10 14:52:57 -0700

    Cleanup wof/flask/config.py

--- wof/flask/config.py
+++ wof/flask/config.py
@@ -1,8 +1,10 @@
 from __future__ import (absolute_import, division, print_function)
 
+
 class Config(object):
     DEBUG = False
     TESTING = False
+
 
 class DevConfig(Config):
     DEBUG = True
